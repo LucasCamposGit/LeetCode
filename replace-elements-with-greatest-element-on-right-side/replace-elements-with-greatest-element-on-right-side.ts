874223d7b02dc3270f7490995bb2211962620f46
LucasCamposGit <lucascpn2501@gmail.com>
2023-09-01 14:30:03 -0300

    updates

--- replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.ts
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.ts
@@ -1,4 +1,19 @@
 namespace replaceElements {
+    // Given an array arr, replace every element in that 
+    // array with the greatest element among the elements to its right, 
+    // and replace the last element with -1.
+
+    // After doing so, return the array.
+    
+     
+    
+    // Example 1:
+    
+    // Input: arr = [17,18,5,4,6,1]
+    // Output: [18,6,6,6,1,-1]
+
+
+
     function replaceElements(arr: number[]): number[] {
         /* arr[0] = max(arr[1], max(1)) */
     
